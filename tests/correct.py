5d6b384a2f1b8caa9421b428e5d81aaa1d9a82e1
Christoph Deil <Deil.Christoph@gmail.com>
2012-02-24 20:11:31 +0100

    Add first tests. disk and ring masks for simple cases.

--- tests/correct.py
+++ tests/correct.py
@@ -1,1 +1,25 @@
 """Check ringtophat results against scipy.ndimage.convolve"""
+import unittest
+from numpy.testing import assert_equal, assert_almost_equal
+import numpy as np
+import ringtophat
+
+class TestKernels(unittest.TestCase):
+    def test_binary_disk(self):
+        actual = ringtophat.binary_disk(1)
+        desired = np.array([[False, True, False],
+                            [ True, True, True],
+                            [False, True, False]])
+        assert_equal(actual, desired)
+    def test_binary_ring(self):
+        actual = ringtophat.binary_ring(1, 2)
+        desired = np.array([[False, False, True, False, False],
+                            [False, True, True, True, False],
+                            [ True, True, False, True, True],
+                            [False, True, True, True, False],
+                            [False, False, True, False, False]])
+        assert_equal(actual, desired)
+
+if __name__ == '__main__':
+    unittest.main()
+
